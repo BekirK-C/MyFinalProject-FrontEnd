9cc56aba12921b0a5702c50e8b0e46e73423755f
BekirK-C <b.b.kamac@gmail.com>
2022-10-26 20:01:02 +0300

    Extensions

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  product1: any = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 5 }
+  product2: any = { productId: 2, productName: "Laptop", categoryId: 1, unitPrice: 25 }
+  product3: any = { productId: 3, productName: "Keyboard", categoryId: 2, unitPrice: 15 }
+  product4: any = { productId: 4, productName: "Mouse", categoryId: 2, unitPrice: 50 }
+  product5: any = { productId: 5, productName: "Camera", categoryId: 3, unitPrice: 45 }
 
+  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
   constructor() { }
 
   ngOnInit(): void {
